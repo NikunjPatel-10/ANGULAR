21a72ee747616a13723feec639447e24af3df043
Nikunj patel <nikunj199842@gmail.com>
2022-09-27 22:18:52 +0530

    27/09/2022

--- Project-format/my-project/src/app/user/home/home.component.ts
+++ Project-format/my-project/src/app/user/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Subject, BehaviorSubject, ReplaySubject, AsyncSubject } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  public subject$ = new Subject();
+  constructor() {
 
-  constructor() { }
+  }
 
   ngOnInit(): void {
+    let observer = this.subject$.subscribe({
+      next: (v) => {
+        console.log(`observerA: ${v}`)
+      },
+
+    })
+
+    this.subject$.next("one");
+    this.subject$.next("two");
+    // this.subject$.complete();
+
+    this.subject$.subscribe({
+      next: (v) => {
+        console.log(`observerB : ${v}`)
+      }
+    })
+
+    this.subject$.next("three");
+
+
+
+
   }
+
 
 }
