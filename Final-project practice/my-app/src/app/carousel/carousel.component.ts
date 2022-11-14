e567cb95dd95c073e062ff17a3b549ef669f860d
Nikunj Patel <nikunj.patel@1Rintern.local>
2022-11-14 17:56:14 +0530

    14-11-2022

--- Final-project practice/my-app/src/app/carousel/carousel.component.ts
+++ Final-project practice/my-app/src/app/carousel/carousel.component.ts
@@ -15,59 +15,92 @@ export class CarouselComponent implements OnInit {
   public artistAllData: any = []
   public lastFiveData: any = []
   public artisFiveData: any = []
-  constructor(private imageservice: ImageService) { }
+
+  public studioId!: number
+  /**
+   * 
+   * @param imageservice 
+   */
+  constructor(private imageservice: ImageService) {
+
+  }
 
 
 
   ngOnInit(): void {
     this.CarouselData();
-    this.artistData()
-    // this.artistData();
-    // this.dhsjh()
-    // this.mergeImage = this.lastFourImage.concat(this.lastArtistImage)
+    this.artistData();
+    this.ProfileData();
+
   }
 
+
+
+  /**
+   * to show the lastest two data of artist and studio in carosuel 
+   */
   CarouselData() {
     this.imageservice.getCarouselData().subscribe(res => {
-      this.carouselAllData = res;
-
       // get last-two carouselData from Database
+      // this.carouselAllData = res.slice((res.length - 2), res.length).reverse();
+      this.carouselAllData = res.slice(-2).reverse();
+      console.log(this.carouselAllData);
 
-      this.lastTwoImage = this.carouselAllData.slice((this.carouselAllData.length - 2), this.carouselAllData.length).reverse();
-      // console.log(this.lastFourImage);
 
       this.imageservice.getArtistData().subscribe(res => {
-        this.artistAllData = res;
-
         // get last-two ArtistData from Database
-        this.lastArtistImage = this.artistAllData.slice((this.artistAllData.length - 2), this.artistAllData.length).reverse();
+        // this.artistAllData = res.slice((res.length - 2), res.length).reverse();
+        this.artistAllData = res.slice(-2).reverse();
 
         // merge twoArray to show Both Data
 
-        this.mergeImage = this.lastTwoImage.concat(this.lastArtistImage)
-        // console.log(this.mergeImage);
+        this.mergeImage = (this.carouselAllData.concat(this.artistAllData));
+        console.log(this.mergeImage);
+        this.shuffleArray(this.mergeImage)
+
       })
-      // this.mergeImage = this.lastFourImage.concat(this.lastArtistImage)
-      // console.log(this.mergeImage);
 
     })
-
-
-    // this.imageservice.getArtistData().subscribe(res => {
-    //   this.artistAllData = res;
-    //   this.lastArtistImage = this.artistAllData.slice((this.artistAllData.length - 2), this.artistAllData.length)
-    // })
-    // this.mergeImage = this.lastFourImage.concat(this.lastArtistImage)
 
 
   }
 
+  // two show the data randomly in the carousel
+  /**
+   * 
+   * @param arr 
+   */
+  shuffleArray(arr: any) {
+    for (var i = arr.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+    }
+  }
+
+  /**
+   * two show the latest artist data
+   */
+
   artistData() {
     this.imageservice.getArtistData().subscribe(res => {
-      this.artisFiveData = res;
-      this.lastFiveData = this.artisFiveData.slice((this.artisFiveData.length - 5), this.artisFiveData.length).reverse()
+      this.artisFiveData = res.slice(-5).reverse();
+      // this.lastFiveData = this.artisFiveData.slice((this.artisFiveData.length - 5), this.artisFiveData.length).reverse()
+      // this.lastFiveData = this.artisFiveData.slice(-5).reverse()
+      // console.log(this.lastFiveData);
     })
     console.log(this.carouselAllData['img']);
+  }
+
+
+
+  ProfileData() {
+    this.imageservice.getCarouselData().subscribe(res => {
+      this.lastFiveData = res.find(x => x.id === this.studioId)
+      console.log(this.lastFiveData);
+
+    })
   }
 
 
